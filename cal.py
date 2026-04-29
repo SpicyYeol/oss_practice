8a319d9a4beabe419f329cddf3181497ed2b6f84
Dae-Yeol Kim <spicyyeol@gmail.com>
2026-04-29 10:36:00 +0900

    feat:ADD 함수화

--- cal.py
+++ cal.py
@@ -1,9 +1,12 @@
 ### 사칙연산
+def add(num1,num2):
+    return num1+num2
+
 num1 = int(input("num1:"))
 num2 = int(input("num2:"))
 op = input("op:")
 
-if op == '+': print(num1+num2)
+if op == '+': print(add(num1,num2))
 elif op == '-': print(num1-num2)
 elif op == '*': print(num1*num2)
 elif op == '/': print(num1/num2)
